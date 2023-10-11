4442be272da7d263893d9135d12a935c98dc5b56
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-10-10 22:13:14 -0300

    Front proyecto entregable

---
+++ src/components/lista.jsx
@@ -0,0 +1,30 @@
+import React,{useState, useEffect} from "react";
+
+const Lista = ()=>{
+
+    //const personas= ['Leandro','Gaston','Jorge','Emilio'];
+
+    const[personas,setPersonas] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:3001/lista')
+        .then(resp => resp.json())
+        .then(data => setPersonas(data))
+        .catch(error =>{
+            console.log('no se pudo obtener las personas',error)
+        });
+    },[]);
+
+    return(
+    <>
+        <h1>Personas</h1>
+        <ul>
+            {personas.map((persona,index) => (
+                <li key={index}>{persona.nombre} - {persona.apellido} - {persona.nacionalidad}</li>
+            ))}
+        </ul>
+    </>
+    )
+}
+
+export default Lista;
